80925995a7776e9f527d96f3a1cb64a9bfd5323f
shinara03 <shinara03@gmail.com>
2020-12-04 12:13:43 -0800

    define parent= method

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -8,9 +8,30 @@ class PolyTreeNode
   end
 
   def parent=(node)
-    if self.parent == nil
-      self.parent = node.parent
-      self.children << node.children
+    if node == nil
+      @parent = node
+    else
+      if !@parent.nil?
+        @parent.children.delete(self) 
+        @parent = node
+        node.children << self.value
+      else
+        @parent = node
+        node.children << self.value
+      end
     end
+
+    # if node == nil
+    #   self.parent = node
+    # else
+    #   if !self.parent.nil?
+    #     self.parent.children.delete(self) 
+    #     self.parent = node
+    #     node.children << self.value
+    #   else
+    #     self.parent = node
+    #     node.children << self.value
+    #   end
+    # end
   end
 end
