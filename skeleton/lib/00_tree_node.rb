8a684e3568a551d5c27ed08d07add72ebb991b2a
Brandon Estano <estanob@gmail.com>
2020-12-04 11:55:56 -0800

    Initialize PolyTreeNode class

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,16 @@
 class PolyTreeNode
+  attr_reader :parent, :value, :children
 
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(node)
+    if self.parent == nil
+      self.parent = node.parent
+      self.children << node.children
+    end
+  end
 end
