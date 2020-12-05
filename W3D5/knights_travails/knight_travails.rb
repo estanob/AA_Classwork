d643d665cad5b89937bdbf9a627098037de0a66d
shinara03 <shinara03@gmail.com>
2020-12-04 17:00:38 -0800

    continue writing class method::self.valid_moves

--- W3D5/knights_travails/knight_travails.rb
+++ W3D5/knights_travails/knight_travails.rb
@@ -3,20 +3,24 @@ class KnightPathFinder
   
   def self.valid_moves(pos)
     @considered_positions = [pos]
-    8_moves = [[-1, -2], [-2, -1], [-2, 1], [-1, 2], [1, 2], [1, -2], [2, -1], [2, 1]]
-    8_moves.each do |move|
+    #possible_moves = []
+    moves = [[-1, -2], [-2, -1], [-2, 1], [-1, 2], [1, 2], [1, -2], [2, -1], [2, 1]]
+    moves.each do |move|
       move_pos = [move[0] + pos[0], move[1] + pos[1]]
       #[-1, -2] [0, 0] move[0] + pos[0]       move[1] + pos[1]
       if move_pos[0] >= 0 && move_pos[0] < 8 && move_pos[1] >= 0 && move_pos[1] < 8
-        @considered_positions.push(move_pos)
+        #@considered_positions.push(move_pos)
+        #possible_moves.push(move_pos)
       end
     end
+    #possible_moves [[3,2], [2,3], ...]
   end
 
   def initialize(starting_pos)
     @starting_pos = starting_pos
     @root_node = PolyTreeNode.new(starting_pos)
-    @board = Array.new(8) {Array.new(8)}
+    @board = Array.new(8) {Array.new(8,"-")}
+    self.build_move_tree(starting_pos)
     #    0 1 2 3 4 5 6 7
     #  0 
     #  1
@@ -28,13 +32,13 @@ class KnightPathFinder
     #  7
   end
 
-  def build_move_tree
+  def build_move_tree(pos)
 
   end
 
   def new_move_positions(pos)
     if !@considered_positions.include?(pos)
-      KnightPathFinder.valid_moves(pos)
+      #possible_moves = KnightPathFinder.valid_moves(pos)
       @considered_positions.push(pos)
     end
     pos
