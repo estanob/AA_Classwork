4b58c371592970d196d9e94a95cc1869524c3edf
Brandon Estano <estanob@gmail.com>
2020-12-09 11:26:59 -0800

    Complete Board#move_piece method

--- W4D2/chess_part_one/board.rb
+++ W4D2/chess_part_one/board.rb
@@ -2,7 +2,8 @@ require_relative "piece.rb"
 require_relative "nullpiece.rb"
 
 class Board
-
+  attr_accessor :board
+  
   def initialize
     @board = Array.new(8) { Array.new(8) { Piece.new } }
     @board[2..5].each_with_index do |row, idx_1| 
@@ -22,6 +23,16 @@ class Board
     @board[row][col] = value
   end
 
+# def [](pos)
+#     row, col = pos
+#     @grid[row][col]
+# end
+
+# def []=(pos, val)
+#     row, col = pos
+#     @grid[row][col] = val [[1,0]]
+# end
+
   def valid_move?(starting_pos, end_pos)
     # [2, 3] => [5, 6] => false
     # [+3, +3] [+-2, +-1] OR [+-1, +-2]
@@ -32,18 +43,14 @@ class Board
   end
 
   def move_piece(starting_pos, end_pos)
-    if [starting_pos].is_a?(NullPiece)
-    # if @board[starting_pos].is_a?(NullPiece)
+    if self[starting_pos].is_a?(NullPiece)
       raise "There is no piece at this starting position"
     elsif !valid_move?(starting_pos, end_pos)
       raise "You cannot move to that end position" 
     else #valid move
       p "You have moved your piece from #{starting_pos} to #{end_pos}"
-      # remove piece from starting_pos, add_pice to end_pos
-      [end_pos] = [starting_pos]
-      [starting_pos] = NullPiece.new
-      # @board[end_pos] = @board[starting_pos]
-      # @board[starting_pos] = NullPiece.new
+      self[end_pos] = self[starting_pos]
+      self[starting_pos] = NullPiece.new
       return true
     end
     
@@ -54,7 +61,7 @@ b = Board.new
 p b[[1,0]]
 p b[[2,0]]
 p "check if the move worked"
-b.move_piece([1, 0], [2, 0])
+b.move_piece([2, 0], [2, 0])
 p b[[1,0]]
 p b[[2,0]]
 # @board[[1, 0]]
