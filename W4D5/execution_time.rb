00e6d203aae4ee97db4a248489c12606eb29b437
Brandon Estano <estanob@gmail.com>
2020-12-11 12:15:58 -0800

    Complete largest_contiguous_sum phase I

--- W4D5/execution_time.rb
+++ W4D5/execution_time.rb
@@ -9,6 +9,7 @@ def my_min(arr)
   end
   smallest
 end
+
 #phase I: quadratic O(n^2)
 
 
@@ -21,61 +22,58 @@ def my_min_2(arr)
   end
   smallest
 end
+
 # phase II: O(n)
+
 # list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 # p my_min_2(list)  # =>  -5
 
 
 def largest_contiguous_subsum(arr)
-  return arr.sum if arr.length <= 2
-  if arr.all? {|el| el < 0 }
-    sums = arr.min
-    subarrays = []
-    (0...arr.length).each do |i|
-      (0...arr.length).each do |j|
-        if j > i 
-      subarrays << arr[i..j]
-      end
-    end
-    end
-  
-     subarrays.each do |sub|
-      if sub.sum > sums
-       sums = sub.sum
-      end
-    end
-  end
-  #array.all? if all elements are < 0, make sum be whatever is the greatest
-  #or for placeholder, sums be a name for that array for comparison
-
-  sums = 0
+  sums = nil
   subarrays = []
   (0...arr.length).each do |i|
-    (0...arr.length).each do |j|
+    (0...arr.length).each do |j|  #n^2
       if j > i 
-        subarrays << arr[i..j]
+        subarrays << arr[i..j] # <= iteration, n 
       end
     end
   end
   
   subarrays.each do |sub|
-    if sub.sum > sums
+    if sums == nil || sub.sum > sums  #n^2
       sums = sub.sum
     end
   end
-  
-  sums
-  # sums = nil
-  # (0...arr.length - 1).each_with_index do |el, i|
-  #   if (arr[i] + arr[i + 1] > sums) || sums == nil
-  #     sums = arr[i] + arr[i + 1]
-  #   end
-  # end
-  # sums
-end
 
-list = [-5, -4, -7]
-p largest_contiguous_subsum(list) # => 8
+  sums
+end
+# n^2 + n^2
+
+# Phase I: O(n^2) n^3
+
+
+def largest_contiguous_subsum_2(arr)
+  sums = nil
+  check_sum = nil
+  (0...arr.length - 1).each do |i|    #n
+    if check_sum == nil
+      check_sum = arr[i]
+      # get nil case on first if
+    elsif check_sum == nil || check_sum < arr[i] + arr[i + 1]
+      #check if el is bigger or el & after is bigger
+      check_sum = arr[i] + arr[i + 1]
+    end
+    if sums == nil || sums < check_sum
+      sums = check_sum
+    end 
+  end
+
+  sums
+end
+# Phase II: O(n)
+list = [-5, -4, -7] # => -4
+p largest_contiguous_subsum_2(list) # => 8
 
 # possible sub-sums
 [5]           # => 5
