4196a160c4b3383a5cea97031cf60b34f8621e73
Justin Cheasty <cheasty@gmail.com>
2020-12-11 11:46:32 -0800

    Passed largest_contiguous_sum now doing bonus of it

--- W4D5/execution_time.rb
+++ W4D5/execution_time.rb
@@ -27,22 +27,60 @@ end
 
 
 def largest_contiguous_subsum(arr)
-  sums = nil
-  (0...arr.length - 1).each_with_index do |el, i|
-    if (arr[i] + arr[i + 1] > sums) || sums == nil
-      sums = arr[i] + arr[i + 1]
+  return arr.sum if arr.length <= 2
+  if arr.all? {|el| el < 0 }
+    sums = arr.min
+    subarrays = []
+    (0...arr.length).each do |i|
+      (0...arr.length).each do |j|
+        if j > i 
+      subarrays << arr[i..j]
+      end
+    end
+    end
+  
+     subarrays.each do |sub|
+      if sub.sum > sums
+       sums = sub.sum
+      end
     end
   end
+  #array.all? if all elements are < 0, make sum be whatever is the greatest
+  #or for placeholder, sums be a name for that array for comparison
+
+  sums = 0
+  subarrays = []
+  (0...arr.length).each do |i|
+    (0...arr.length).each do |j|
+      if j > i 
+        subarrays << arr[i..j]
+      end
+    end
+  end
+  
+  subarrays.each do |sub|
+    if sub.sum > sums
+      sums = sub.sum
+    end
+  end
+  
   sums
+  # sums = nil
+  # (0...arr.length - 1).each_with_index do |el, i|
+  #   if (arr[i] + arr[i + 1] > sums) || sums == nil
+  #     sums = arr[i] + arr[i + 1]
+  #   end
+  # end
+  # sums
 end
 
-list = [5, 3, -7]
+list = [-5, -4, -7]
 p largest_contiguous_subsum(list) # => 8
 
 # possible sub-sums
-# [5]           # => 5
-# [5, 3]        # => 8 --> we want this one
-# [5, 3, -7]    # => 1
-# [3]           # => 3
-# [3, -7]       # => -4
-# [-7]          # => -7
+[5]           # => 5
+[5, 3]        # => 8 --> we want this one
+[5, 3, -7]    # => 1
+[3]           # => 3
+[3, -7]       # => -4
+[-7]          # => -7
