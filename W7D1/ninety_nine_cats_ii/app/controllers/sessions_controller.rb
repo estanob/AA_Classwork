735e915e20db36fbb58e72dca0cf2fc59d991039
Bogey <jeffreysbogart@gmail.com>
2021-01-04 14:42:24 -0800

    Creates controllers for user and session

--- W7D1/ninety_nine_cats_ii/app/controllers/sessions_controller.rb
+++ W7D1/ninety_nine_cats_ii/app/controllers/sessions_controller.rb
@@ -1,2 +1,15 @@
 class SessionsController < ApplicationController
+
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def create
+        @user = User.find_by_credentials(params[:user][:user_name], params[:user][:password])
+        @user.reset_session_token!
+
+        redirect_to cats_url
+    end
+
 end
--- W7D1/ninety_nine_cats_ii/config/routes.rb
+++ W7D1/ninety_nine_cats_ii/config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
   root to: redirect('/cats')
 
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
 end
