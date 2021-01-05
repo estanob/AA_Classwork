06b8b55ebe0c511238c71dff586d49ef73602770
Andrew Curd <andrewc2695@gmail.com>
2021-01-05 11:07:39 -0800

    wrote new method in users controller, started on partial for users

--- W7D2/music_app/music_app/app/models/user.rb
+++ W7D2/music_app/music_app/app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
 
   validates :username, :session_token, uniqueness: true, presence: true
   validates :password_digest, presence: true
+  validates :password, length: {minimum: 6}, allow_nil: true
+
+  after_initialize :ensure_session_token
 
   def self.find_by_credentials(username, pw)
     user = User.find_by(username: username)
@@ -14,11 +17,12 @@ class User < ApplicationRecord
   end
 
   def password=(password)
-    password_digest = BCrypt::Password.create(password)
+    self.password_digest = BCrypt::Password.create(password)
+    @password = password
   end
 
   def is_password?(pw)
-    
+    BCrypt::Password.new(self.password_digest).is_password?(pw)
   end
 
   def reset_session_token!
