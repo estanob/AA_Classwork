5b8a50dbf20703c7aa61ccc2ac83ec8f3a0f1d76
Brandon Estano <estanob@gmail.com>
2021-01-05 14:45:42 -0800

    Begin CSRF protection

--- W7D2/music_app/music_app/app/controllers/bands_controller.rb
+++ W7D2/music_app/music_app/app/controllers/bands_controller.rb
@@ -1,14 +1,45 @@
 class BandsController < ApplicationController
-    def new
-        @band = Band.new
-        render :new
+  # skip_before_action :verify_authenticity_token
+
+  def new
+    @band = Band.new
+    render :new
+  end
+
+  def index 
+    @bands = Band.all
+    render :index
+  end
+
+  def show
+    @band = Band.find(params[:id])
+    render :show
+  end
+
+  def create
+    @band = Band.new(band_params)
+    if @band.save
+      redirect_to band_url(@band.id)
+    else
+      render :new
     end
+  end
 
-    def index 
-        @bands = Band.all
-        render :index
-    end
+  def edit
+    @band = Band.find(params[:id])
+    render :edit
+  end
 
+  def destroy
+    @band = Band.find(params[:id])
+    @band.destroy
+    render :index
+  end
 
+  private
+
+  def band_params
+    params.require(:band).permit(:name)
+  end
 
 end
