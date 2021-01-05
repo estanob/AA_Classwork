4ec2010a892be5f5956129e1e17b3b052b3f5f53
Andrew Curd <andrewc2695@gmail.com>
2021-01-05 14:28:53 -0800

    created band table, model, controller, and routes

--- W7D2/music_app/music_app/app/controllers/bands_controller.rb
+++ W7D2/music_app/music_app/app/controllers/bands_controller.rb
@@ -1,2 +1,14 @@
 class BandsController < ApplicationController
+    def new
+        @band = Band.new
+        render :new
+    end
+
+    def index 
+        @bands = Band.all
+        render :index
+    end
+
+
+
 end
