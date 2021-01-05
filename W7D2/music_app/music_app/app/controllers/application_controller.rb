fee3a8b23ac71a2e974c67c8e8e07d13e0add045
Brandon Estano <estanob@gmail.com>
2021-01-05 11:23:29 -0800

    Complete new user form

--- W7D2/music_app/music_app/app/controllers/application_controller.rb
+++ W7D2/music_app/music_app/app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  
+  def login
+
+  end
+  
 end
--- W7D2/music_app/music_app/app/controllers/users_controller.rb
+++ W7D2/music_app/music_app/app/controllers/users_controller.rb
@@ -3,4 +3,18 @@ class UsersController < ApplicationController
         @user = User.new
         render :new
     end
+
+    def create
+        @user = User.new(user_params)
+        @user.save
+        if @user
+            
+        else 
+            render :new
+        end
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
