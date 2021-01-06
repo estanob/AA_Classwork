69f3ad7da27cbd29b8b59e8192d7eac8a801077f
Brandon Estano <estanob@gmail.com>
2021-01-06 14:44:46 -0800

    Write tests for User model methods

--- W7D3/goal_app/spec/models/user_spec.rb
+++ W7D3/goal_app/spec/models/user_spec.rb
@@ -29,5 +29,35 @@ RSpec.describe User, type: :model do
       user = User.find_by(username: 'Carlos')
       expect(user.password).not_to eq('password')
     end
+
+    it 'ensures password is encrypted using BCrypt' do
+      expect(BCrypt::Password).to receive(:create).with('123456')
+      FactoryBot.build(:user, password: '123456')
+    end
+  end
+
+  describe '::find_by_credentials' do
+    context 'when params are valid' do
+      it 'will find user given the username and correct password' do
+        user = FactoryBot.create(:user, username: 'Carlos', password: '123456') #.create will add to DB
+        expect(User.find_by_credentials('Carlos', '123456')).to eq(user)
+      end
+    end
+
+    context 'when params are not valid' do
+      it 'returns nil' do
+        expect(User.find_by_credentials('Carlos', 'abcdef')).to eq(nil)
+      end
+    end
+  end
+
+  describe '#password=' do
+    it '' do 
+
+    end
+
+    it 'creates password_digest for user using ' do 
+
+    end
   end
 end
