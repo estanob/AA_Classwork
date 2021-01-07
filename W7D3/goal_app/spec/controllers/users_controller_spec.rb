085c79f978fc0e4d3d37c36cb3be773eb529599b
Janette <tinoconjanette@gmail.com>
2021-01-06 16:23:24 -0800

    Added user_controller test for new. show, and edit

--- W7D3/goal_app/spec/controllers/users_controller_spec.rb
+++ W7D3/goal_app/spec/controllers/users_controller_spec.rb
@@ -3,16 +3,33 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
   #edit, show, destroy
   describe 'GET #new' do
-    get :new
-    expect(response).to be_success
-    expect(response).to render_template(:new)
+    it 'renders new users template successfully' do
+      get :new
+      expect(response).to be_success
+      expect(response).to render_template(:new)
+    end
   end
 
   describe 'GET #show' do
     #how to find a user by indexing id
-    get :show, id: ???
-    expect(response).to be_success
-    expect(response).to render_template(:show)
+    #since we have Carlos, his id will be 1
+    #expect look up to show carlos' page
+    it 'renders the show template' do
+      User.create!(username: 'Zack', password: 'piano1')
+      get :show, id: 1
+      expect(response).to render_template(:show)
+    end
+
+    context 'if the user does not exist' do
+      it 'is not a success' do
+        begin
+          get :show, id: -1
+        rescue
+          ActiveRecord::RecordNotFound
+        end
+        expect(response).not_to render_template(:show)
+      end
+    end
   end
 
   describe 'POST #edit' do
