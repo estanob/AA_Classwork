8dba1a3a76742c148ffa95a5c2669aa109aa80b4
Brandon <estanob@gmail.com>
2021-01-13 13:58:33 -0800

    Continue Array#twoSum

---
+++ W8D3/intro_to_js/phase_1_arrays.js
@@ -0,0 +1,27 @@
+Array.prototype.uniq = function(){
+  let uniq_arr = [];
+  this.forEach(function(ele){
+    if (!uniq_arr.includes(ele)){
+      uniq_arr.push(ele);
+    }
+  })
+  return uniq_arr;
+}
+
+// console.log([1, 2, 2, 3, 3, 3].uniq())
+
+
+
+Array.prototype.twoSum = function(){
+  let pairs = [];
+  for (let i = 0; i < this.length; i++){
+    for (let j = 0; j < this.length; j++){
+      if (this[i] + this[j] === 0) ^ (j > i){
+        pairs.push([i, j])
+      }
+    }
+  }
+  return pairs;
+}
+
+console.log([-1, 1, -2, 2, 3, 4].twoSum())
