3db64cd4056e60c3a2964792a46b6518fe53d1cb
Brandon <estanob@gmail.com>
2021-01-13 15:31:57 -0800

    Complete Array#myMap

--- W8D3/intro_to_js/phase_2_enumerable.js
+++ W8D3/intro_to_js/phase_2_enumerable.js
@@ -1,15 +1,30 @@
 Array.prototype.myEach = function(cb){
-    this.forEach(function(ele){
-        cb(ele)
-    })
-    
-    return this 
-
-    
-}
-function addTwo(num){
-    return num + 2
+    for (let i = 0; i < this.length; i++){
+        cb(this[i]);
+    }
+    return this;
 }
 
+// let addTwo = function(num) {
+//     console.log(num + 2);
+// }
 
-console.log([4,5,6].myEach(addTwo(num)))
+// const arr = [4, 5, 6] // => [6, 7, 8]
+// arr.myEach(ele) => {console.log(ele + 2)}
+// console.log(arr.myEach(addTwo))
+
+
+Array.prototype.myMap = function(cb){
+    const mapped = [];
+    this.myEach(ele => mapped.push(cb(ele)))
+    return mapped;
+}
+ 
+const arr = [4, 5, 6]
+
+let addTwo = function (num) {
+    console.log(num + 2);
+}
+
+// console.log(arr.myMap(addTwo));
+console.log(arr.myMap(ele => ele + 2));
