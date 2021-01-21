bba5f742046a209c3ceb3a373befabdc3d04b3e1
IsoVoyd <dubnersamuel@gmail.com>
2021-01-21 12:06:13 -0800

    work on FollowToggle class

--- W9D4/ajax twitter/frontend/follow_toggle.js
+++ W9D4/ajax twitter/frontend/follow_toggle.js
@@ -3,6 +3,23 @@ class FollowToggle {
     this.followButton = $ele;
     this.userId = this.followButton.attr('user-id');
     this.followState = this.followButton.attr('initial-follow-state');
+    this.render();
+  }
+
+  render () {
+    if (this.followState === "unfollowed") {
+      this.followButton.val("Follow!");
+    } else {
+      this.followButton.val("Unfollow!");
+    }
+  }
+
+  handleClick () {
+    $.ajax({
+      method: "POST",
+      url: `/users/${userId}/follow`,
+      
+    })
   }
 }
 
