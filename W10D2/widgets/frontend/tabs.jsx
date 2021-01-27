85f381f9f97c049afee10e63dd373fc2691354b2
timjkim210 <timjkim210@gmail.com>
2021-01-26 17:31:53 -0800

    started Tabs class

---
+++ W10D2/widgets/frontend/tabs.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+class Tabs extends React.Component {
+    constructor(props, objs) {
+        super(props);
+        this.state = {
+            title: "",
+            content: ""
+        }
+    }
+}
