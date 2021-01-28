917ed246d83bc8ae431e6b2f41a59b6240caf1e8
Brandon <estanob@gmail.com>
2021-01-28 12:21:50 -0800

    Set up Todos App

---
+++ W10D4/todospt1/frontend/todo_redux.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import configureStore from './store';
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(<h1>Todos App</h1>, document.getElementById('content'))
+})
