b370db301abefd8a24294bb7c435d3da9b74558b
Brandon <estanob@gmail.com>
2021-02-01 15:04:29 -0800

    Set up StaticPagesController

--- W10D4/todo_project/db/seeds.rb
+++ W10D4/todo_project/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Todo.destroy_all
+
+puts "Creating todos..."
+Todo.create!(title: "a", body: "a", done: false)
+Todo.create!(title: "b", body: "b", done: false)
+Todo.create!(title: "c", body: "c", done: false)
+Todo.create!(title: "d", body: "d", done: false)
+Todo.create!(title: "e", body: "e", done: false)
+Todo.create!(title: "f", body: "f", done: false)
+Todo.create!(title: "g", body: "g", done: false)
+Todo.create!(title: "h", body: "h", done: false)
+Todo.create!(title: "i", body: "i", done: false)
+Todo.create!(title: "j", body: "j", done: false)
+
